0648c69c3ab4adc3e5603f79f25f69349acfbf3d
song <thdxorms501@naver.com>
2025-05-27 20:44:55 +0900

    0527_1

--- weather/weather.js
+++ weather/weather.js
@@ -1,5 +1,6 @@
 const weather = document.querySelector("#weather span:first-child");
 const city = document.querySelector("#weather span:last-child");
+
 const API_KEY="48d08bea1ce9acbac1dd9aaae47f5867";
 
 function onGeoOk(position) {
@@ -7,7 +8,8 @@ function onGeoOk(position) {
   const lon = position.coords.longitude;
   //console.log("You live in", lat, lon);
   //console.log(position);
-  const url= `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=mertic`;
+  const url= `https://api.openweathermap.org/data/2.5/weather?
+    lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   //console.log(url);
   fetch(url)
   .then(response => response.json())
